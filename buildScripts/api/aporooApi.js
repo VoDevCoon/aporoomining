28929d98805ae822e5ea8e140e85413b527bead8
VoDevCoon <victorzhouau@gmail.com>
2018-07-24 17:16:20 +1000

    api init

--- buildScripts/api/aporooApi.js
+++ buildScripts/api/aporooApi.js
@@ -1,7 +1,20 @@
+import socket from 'ws';
+import chalk from 'chalk'
+
+//onst ws = new socket('wss://ws.aporoo.com/websocket');
+const ws = new socket('wss://stream.binance.com:9443/ws/bnbzil@aggTrade');
 
 let AporooApi = {
   query : function(action, token, currency){
-    return {"price":100}
+
+    ws.on('open', function(){
+        //ws.send(JSON.stringify({"dataType":"90_ENTRUST_ADD_AT_USDT","dataSize":20,"action":"ADD"}  ));
+        console.log(chalk.yellow("open"));
+    });
+
+    ws.on('message',function(data){
+      console.log('received: ' + data);
+    });
   }
 }
 
