c2038d51b1a8e62972a2167e77bd7694f59c0476
Victor Zhou <victorzhou@192-168-1-107.tpgi.com.au>
2018-07-24 21:47:27 +1000

    first call

--- buildScripts/api/aporooApi.js
+++ buildScripts/api/aporooApi.js
@@ -1,14 +1,14 @@
 import socket from 'ws';
 import chalk from 'chalk'
 
-//onst ws = new socket('wss://ws.aporoo.com/websocket');
-const ws = new socket('wss://stream.binance.com:9443/ws/bnbzil@aggTrade');
+const ws = new socket('wss://ws.aporoo.com/websocket');
+//const ws = new socket('wss://stream.binance.com:9443/ws/zileth@depth20');
 
 let AporooApi = {
   query : function(action, token, currency){
 
     ws.on('open', function(){
-        //ws.send(JSON.stringify({"dataType":"90_ENTRUST_ADD_AT_USDT","dataSize":20,"action":"ADD"}  ));
+        ws.send(JSON.stringify({"dataType":"482_ENTRUST_ADD_AT_ETH","dataSize":50,"action":"ADD"}  ));
         console.log(chalk.yellow("open"));
     });
 
