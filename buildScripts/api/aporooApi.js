2289d6c1ab29303c18df699981fb60ea11d9a4e8
VoDevCoon <victorzhouau@gmail.com>
2018-07-25 17:10:35 +1000

    class init

--- buildScripts/api/aporooApi.js
+++ buildScripts/api/aporooApi.js
@@ -3,9 +3,8 @@ import chalk from 'chalk'
 
 const ws = new socket('wss://ws.aporoo.com/websocket');
 //const ws = new socket('wss://stream.binance.com:9443/ws/zileth@depth20');
-
-let AporooApi = {
-  query : function(action, token, currency){
+class AporooApi {
+  query (action, token, currency){
 
     ws.on('open', function(){
         ws.send(JSON.stringify({"dataType":"482_ENTRUST_ADD_AT_ETH","dataSize":50,"action":"ADD"}  ));
